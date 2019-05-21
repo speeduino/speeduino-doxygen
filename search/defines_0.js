49a11c47212689839140466e957108fa3baf9a53
Travis CI User <travis@example.org>
2019-05-21 21:52:13 +0000

    Deploy code docs to GitHub Pages. Travis build 543
    Commit 105eeba8aa8f2ecc983e2c65ada4a7b61c94e12d

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['adc_5ffilter',['ADC_FILTER',['../sensors_8h.html#a546e9cb3295371234f60dcf3beae19dd',1,'sensors.h']]],
+  ['ae_5fmode_5fmap',['AE_MODE_MAP',['../globals_8h.html#a41b7653dd4515c44c9832f8adc797009',1,'globals.h']]],
+  ['ae_5fmode_5ftps',['AE_MODE_TPS',['../globals_8h.html#a18268c68ac84c9aeb9e88081e1f36b2c',1,'globals.h']]],
   ['afrmappage',['afrMapPage',['../comms_8h.html#a662ceae37f8f17a64d85551afb7889c0',1,'comms.h']]],
   ['afrsetpage',['afrSetPage',['../comms_8h.html#aaee843271a3f806326d7b94fad9b89bd',1,'comms.h']]]
 ];
