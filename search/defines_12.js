7a31446460755a5af9be0276f6abb47a11ac5233
Travis CI User <travis@example.org>
2020-04-20 13:48:26 +0000

    Deploy code docs to GitHub Pages. Travis build 831
    Commit 5366695f962e4110f88ff83af74900e1b140b1bc

--- search/defines_12.js
+++ search/defines_12.js
@@ -4,6 +4,7 @@ var searchData=
   ['valid_5fmap_5fmin',['VALID_MAP_MIN',['../globals_8h.html#a1ac572a4a78896607b1fc34777ea9c1e',1,'globals.h']]],
   ['vemappage',['veMapPage',['../comms_8h.html#ac08555b37f8b765abcf15bb9871c39bf',1,'comms.h']]],
   ['vesetpage',['veSetPage',['../comms_8h.html#ae0859a89352534458f9cef6a2d8c0f36',1,'comms.h']]],
+  ['vss_5fgear_5fhysteresis',['VSS_GEAR_HYSTERESIS',['../sensors_8h.html#a02d55c320840c9f60cf3b53c50ed34d1',1,'sensors.h']]],
   ['vvt_5fload_5fmap',['VVT_LOAD_MAP',['../globals_8h.html#a74475e95c4cc5dcdb39af388d948ac21',1,'globals.h']]],
   ['vvt_5fload_5ftps',['VVT_LOAD_TPS',['../globals_8h.html#a78b4e1fcf55b80b17001ab712a0b27ba',1,'globals.h']]],
   ['vvt_5fmode_5fclosed_5floop',['VVT_MODE_CLOSED_LOOP',['../globals_8h.html#a843289c040527dfeb3fe4846222eb91d',1,'globals.h']]],
