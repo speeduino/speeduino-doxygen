887042b97662ae29833ff4aebac7c4849b03f5db
Travis CI User <travis@example.org>
2020-07-22 01:31:30 +0000

    Deploy code docs to GitHub Pages. Travis build 982
    Commit 5b342d27bd2f61d98d59ccf22477501ffa45eba2

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['wmicontrol',['wmiControl',['../auxiliaries_8h.html#aa5098532802ea899b4c06d301e2571b7',1,'wmiControl():&#160;auxiliaries.ino'],['../auxiliaries_8ino.html#aa5098532802ea899b4c06d301e2571b7',1,'wmiControl():&#160;auxiliaries.ino']]],
   ['writeallconfig',['writeAllConfig',['../storage_8h.html#ac98869315dfb66d34ce1b2015f8afc40',1,'writeAllConfig():&#160;storage.ino'],['../storage_8ino.html#ac98869315dfb66d34ce1b2015f8afc40',1,'writeAllConfig():&#160;storage.ino']]],
   ['writecalibration',['writeCalibration',['../storage_8h.html#ac3efa003544375926eb7d8d88cf19523',1,'writeCalibration():&#160;storage.ino'],['../storage_8ino.html#ac3efa003544375926eb7d8d88cf19523',1,'writeCalibration():&#160;storage.ino']]],
   ['writeconfig',['writeConfig',['../storage_8h.html#a2b3daa967fcf9b48410920143dcb5219',1,'writeConfig(byte):&#160;storage.ino'],['../storage_8ino.html#a2dac7ee796d1f4a9438a1a0a3f0adc70',1,'writeConfig(byte tableNum):&#160;storage.ino']]]
